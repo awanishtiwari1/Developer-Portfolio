70a36f475c0a9e84fb65a7852f2800559f2dd782
Awanish Tiwari <63773588+awanishtiwari1@users.noreply.github.com>
2025-06-08 00:57:43 +0530

    Update twitter.js

--- src/containers/twitter-embed/twitter.js
+++ src/containers/twitter-embed/twitter.js
@@ -1,20 +1,6 @@
-import React, {useContext} from "react";
 import "./twitter.scss";
-import Loading from "../loading/Loading";
 import {twitterDetails} from "../../portfolio";
-import StyleContext from "../../contexts/StyleContext";
-
-const cantDisplayError =
-  "<div className='centerContent'><h2>Can't load? Check privacy protection settings</h2></div>";
 
 export default function Twitter() {
-  if (!twitterDetails.display) {
-    return null;
-  }
-  if (!twitterDetails.userName) {
-    console.error("Twitter username for twitter section is missing");
-  }
-
-
    return null;
 }
